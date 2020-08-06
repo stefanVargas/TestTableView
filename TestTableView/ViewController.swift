7aa438373e7e8648a4ae2c9013a880936f8e8bce
stefanv.demoraes <stefanvargas3@gmail.com>
2020-08-05 21:15:07 -0300

    Initial Commit

---
+++ TestTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTableView
+//
+//  Created by Stefan V. de Moraes on 05/08/20.
+//  Copyright © 2020 Stefan V. de Moraes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
